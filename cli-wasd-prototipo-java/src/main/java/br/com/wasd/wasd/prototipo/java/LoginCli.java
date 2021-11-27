ec292495e22459a6a90eed8d1bb13770ff3fc027
Wesley Paternezi <wesley.paternezi@bandtec.com.br>
2021-11-26 23:11:32 -0300

    cleaning loginCli and removing gitgnore SlackWebhook

--- cli-wasd-prototipo-java/src/main/java/br/com/wasd/wasd/prototipo/java/LoginCli.java
+++ cli-wasd-prototipo-java/src/main/java/br/com/wasd/wasd/prototipo/java/LoginCli.java
@@ -1,47 +1,13 @@
 package br.com.wasd.wasd.prototipo.java;
 
-import br.com.wasd.wasd.prototipo.java.model.DiscoMaquina;
-import br.com.wasd.wasd.prototipo.java.model.Maquina;
 import br.com.wasd.wasd.prototipo.java.model.Pedido;
 import br.com.wasd.wasd.prototipo.java.model.Usuario;
-import br.com.wasd.wasd.prototipo.java.model.dao.DiscoDao;
 import br.com.wasd.wasd.prototipo.java.model.dao.MaquinaDao;
 import br.com.wasd.wasd.prototipo.java.model.dao.PedidoDao;
 import br.com.wasd.wasd.prototipo.java.model.dao.UsuarioDAO;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
-import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.swing.JOptionPane;
-
-import com.github.britooo.looca.api.core.Looca;
-import com.github.britooo.looca.api.group.discos.Disco;
-import com.github.britooo.looca.api.group.discos.DiscosGroup;
-import com.github.britooo.looca.api.group.memoria.Memoria;
-import com.github.britooo.looca.api.group.processador.Processador;
-import com.github.britooo.looca.api.group.processos.ProcessosGroup;
-import com.github.britooo.looca.api.group.sistema.Sistema;
-import com.profesorfalken.jsensors.model.components.Components;
-import com.profesorfalken.jsensors.model.components.Gpu;
-
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.swing.JOptionPane;
-
-import com.github.britooo.looca.api.core.Looca;
-import com.github.britooo.looca.api.group.discos.Disco;
-import com.github.britooo.looca.api.group.discos.DiscosGroup;
-import com.github.britooo.looca.api.group.memoria.Memoria;
-import com.github.britooo.looca.api.group.processador.Processador;
-import com.github.britooo.looca.api.group.processos.ProcessosGroup;
-import com.github.britooo.looca.api.group.sistema.Sistema;
-import com.profesorfalken.jsensors.model.components.Components;
-import com.profesorfalken.jsensors.model.components.Gpu;
 import java.util.Scanner;
 import me.tongfei.progressbar.ProgressBar;
 
@@ -50,14 +16,6 @@ public class LoginCli {
     /**
      * Creates new form Login
      */
-    private Looca looca;
-    private Sistema sistema;
-    private Memoria memoria;
-    private Processador processador;
-    private DiscosGroup grupoDeDiscos;
-    private Components componentes;
-    private Maquina maquina;
-    private ProcessosGroup grupoDeProcessos;
     private PedidoDao pedidoDao;
     private String hostname;
 
